a4431bf249d6e135b5a9207634a3910af4606535
Lucas Barão <lucas.baraom@gmail.com>
2023-07-20 18:54:39 -0300

    implementação das relações

--- app/Models/Time.php
+++ app/Models/Time.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Time extends Model
 {
     use HasFactory;
+    public function jogadores () {
+        return $this->hasMany(Elenco::class);
+    }
+    public function partidas () {
+        return $this->hasMany(Partida::class);
+    }
+
+    public function tecnicos () {
+        return $this->hasMany(Tecnico::class);
+    }
+
+    public function ligas () {
+        return $this->belongsToMany(Liga::class, 'ligas_times', 'time_id', 'liga_id');
+    }
 }
